fc732ac1e4bb5cfd0877987f414c297b5314aae8
fumi1096 <yilizoo@outlook.com>
2026-04-29 14:59:52 +0800

    完善启动文件

--- car/src/start.sh
+++ car/src/start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+cd "$SCRIPT_DIR"
+
 source /opt/tros/humble/setup.bash
 source ./install/setup.bash
 ros2 run yahboomcar_bringup Mcnamu_driver_X3
