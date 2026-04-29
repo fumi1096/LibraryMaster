6f1a5cecf1c8b83d15c0ab0676117d7f417dea42
fumi1096 <yilizoo@outlook.com>
2026-04-29 13:28:56 +0800

    构建小车

--- car/src/build.sh
+++ car/src/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source /opt/tros/humble/setup.bash
-colcon build --symlink-install --packages-select car
+colcon build
---
+++ car/src/set.sh
@@ -0,0 +1,3 @@
+source /opt/tros/humble/setup.bash
+source ./install/setup.bash
+ros2 run yahboomcar_bringup Mcnamu_driver_X3
